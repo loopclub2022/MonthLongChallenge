b0dd8a3ecb10d721a17ec257da31a74ff798b192
kaustubh1412 <63237045+kaustubh1412@users.noreply.github.com>
2022-05-02 14:01:58 +0530

    add file

---
+++ Kaustubh_19gcebit088/Day1.java
@@ -0,0 +1,26 @@
+public class Day1<ans> {
+    public static void main(String[] args) {
+        String a = "ab#c";
+        String b = "ab#c";
+        System.out.println(getStr(a));
+    }
+
+    static String getStr(String a) {
+        char[] ch = (a).toCharArray();
+        String ans = "";
+        int len = ch.length - 1;
+        int i = 0;
+        while (i <= len) {
+            if (ch[i] != '#') {
+                ans += ch[i];
+
+            } else if (ans.length() != 0) {
+                ans = ans.substring(0, ans.length() - 1);
+            }
+            ++i;
+
+        }
+        return ans;
+    }
+}
+
