bcc30eb647eb30c6aca0111388243cb6c914ff71
Riya Gupta <61340238+Caffeine-Overflows@users.noreply.github.com>
2022-05-03 18:48:14 +0530

    Update Day2_sort_array_by_parity.java

--- Riya - 19 IT/Day2_sort_array_by_parity.java
+++ Riya - 19 IT/Day2_sort_array_by_parity.java
@@ -1,5 +1,11 @@
 //   Question - https://leetcode.com/problems/sort-array-by-parity/
 
+/* Time Complexity - O(N)
+   Space Complexity - O(1)
+   Explanation - Two pointer approach, where we traverse from left till we get a odd number 
+                 and then from right till we get an even number and swap them 
+                 and continue this, till our left and right pointer are pointing at same value.*/
+
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int i =0, j = nums.length-1, temp;
