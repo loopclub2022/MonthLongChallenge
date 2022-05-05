159eb8c6bae806b427d0cd8809de28c4526a8878
kaustubh1412 <63237045+kaustubh1412@users.noreply.github.com>
2022-05-06 00:00:41 +0530

    add time, space complexity and approach

--- Kaustubh_19gcebit088/Day1.java
+++ Kaustubh_19gcebit088/Day1.java
@@ -1,5 +1,17 @@
 // https://leetcode.com/problems/backspace-string-compare/
 
+// time complexity = O(n)
+// space complexity = O(1)
+
+// approach:
+// traverse through the whole char arr 
+// for every element which is not '#'
+//     add it to ans string
+// and for every element which is '#', 
+//     remove the last added element from ans string only if it is not empty
+
+
+
 public class Day1<ans> {
     public static void main(String[] args) {
         String a = "ab#c";
