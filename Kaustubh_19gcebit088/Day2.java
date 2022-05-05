75b2eb8b4687380c05efaa9514f291e8560def49
kaustubh1412 <63237045+kaustubh1412@users.noreply.github.com>
2022-05-06 00:10:13 +0530

    add details as comments

--- Kaustubh_19gcebit088/Day2.java
+++ Kaustubh_19gcebit088/Day2.java
@@ -1,5 +1,17 @@
 // https://leetcode.com/problems/sort-array-by-parity/
 
+// time complexity = O(n)
+// space complexity = O(1)
+
+// approach:
+
+// iterate through the whole arr
+// swap the even num based on its index of occurence
+// here, 2 occurs for the first time,i.e., 0th time
+// so swap it with arr[0]
+// then 4 occurs for 0+1 = 1st time
+// swap it with arr[1]
+
 import java.util.Arrays;
 
 public class Day2 {
